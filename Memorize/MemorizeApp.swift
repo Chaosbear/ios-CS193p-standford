ff8945eb386fd92eb2d7c389d788c49a2ec4c329
krit sukrit <sukrit@dek-d.com>
2022-01-27 01:25:29 +0700

    first commit t lesson5

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,18 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Sukrit Chatmeeboon on 27/1/2565 BE.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    private let game = EmojiMemoryGame()
+    var body: some Scene {
+        WindowGroup {
+            EmojiMemoryGameView(game: game)
+        }
+    }
+}
---
+++ Memorize/MemoryGame.swift
@@ -0,0 +1,62 @@
+//
+//  MemoryGame.swift
+//  Memorize
+//
+//  Created by Non Monthon on 25/1/2565 BE.
+//
+
+import Foundation
+
+struct MemoryGame<CardContent> where CardContent: Equatable {
+    
+    private(set) var cards: Array<Card>
+    
+    private var indexOfFirstFaceUpCard: Int? {
+        get { cards.indices.filter { cards[$0].isFaceUp }.oneAndOnly }
+        set { cards.indices.forEach { cards[$0].isFaceUp = ($0 == newValue) } }
+    }
+    
+    mutating func choose (_ card: Card) {
+        if let chosenIndex = cards.firstIndex(where: { $0.id == card.id}),
+            !cards[chosenIndex].isFaceUp,
+            !cards[chosenIndex].isMatch {
+            if let potentialMatchIndex = indexOfFirstFaceUpCard {
+                if cards[chosenIndex].content == cards[potentialMatchIndex].content {
+                    cards[chosenIndex].isMatch = true
+                    cards[potentialMatchIndex].isMatch = true
+                }
+                cards[chosenIndex].isFaceUp = true
+            } else {
+                indexOfFirstFaceUpCard = chosenIndex
+            }
+        }
+    }
+
+    
+    init (numberOfPairsOfCards: Int, createCardContent: (Int) -> CardContent) {
+        cards = []
+        
+        for pairIndex in 0..<numberOfPairsOfCards {
+            let content = createCardContent(pairIndex)
+            cards.append(Card(content: content, id: pairIndex*2))
+            cards.append(Card(content: content, id: pairIndex*2+1))
+        }
+    }
+    
+    struct Card: Identifiable {
+        var isFaceUp = false
+        var isMatch = false
+        let content: CardContent
+        let id: Int
+    }
+}
+
+extension Array {
+    var oneAndOnly: Element? {
+        if count == 1 {
+            return first
+        } else {
+            return nil
+        }
+    }
+}
